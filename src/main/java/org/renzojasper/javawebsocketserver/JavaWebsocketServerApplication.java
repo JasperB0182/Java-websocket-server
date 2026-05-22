217a7fa9f5611fd19db9f86ae0a706fee79701cd
s1155772 <jbatenburg@ziggo.nl>
2026-05-22 20:52:12 +0200

    chore: Setup java springboot

---
+++ src/main/java/org/renzojasper/javawebsocketserver/JavaWebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package org.renzojasper.javawebsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebsocketServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebsocketServerApplication.class, args);
+    }
+
+}
