25d18444c10569dbbc085c0179cfb2cd5476dbd2
renzos2007 <Renzo@stelleronline.nl>
2026-05-24 21:31:54 +0200

    feat: basic authentication to the websocket.

--- src/main/java/org/renzojasper/javawebsocketserver/configuration/SessionConfig.java
+++ src/main/java/org/renzojasper/javawebsocketserver/configuration/SessionConfig.java
@@ -1,14 +1,7 @@
 package org.renzojasper.javawebsocketserver.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.MapSessionRepository;
-import org.springframework.session.SessionRepository;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
-
-import java.time.Duration;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 @EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 3600)
--- src/main/java/org/renzojasper/javawebsocketserver/configuration/WebSocketConfig.java
+++ src/main/java/org/renzojasper/javawebsocketserver/configuration/WebSocketConfig.java
@@ -5,16 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // De url voor de websocket is ws://localhost:8080/api/chat
-        // We moeten oppassen met de origins, we willen zegmaar NIET dat alle websites dit kunnen bereiken..
+        // The url for the websocket is ws://localhost:8080/api/chat
+        // We have to be carefull with the origins, we don't want all websites to be able to do this, so to speak.
         registry
                 .addHandler(new SocketConnectionHandler(), "/chat")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .setAllowedOrigins("*");
     }
 }
