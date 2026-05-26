41dc93c368c94febf8b3e6d80f7e50dfcd9fcace
renzos2007 <Renzo@stelleronline.nl>
2026-05-26 12:19:00 +0200

    feat: fixed the creation from the missing tables without aan schema.sql

--- src/main/java/org/renzojasper/javawebsocketserver/configuration/SessionConfig.java
+++ src/main/java/org/renzojasper/javawebsocketserver/configuration/SessionConfig.java
@@ -1,9 +1,28 @@
 package org.renzojasper.javawebsocketserver.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 3600)
 public class SessionConfig {
+    @Bean
+    public DataSourceInitializer sessionSchemaInitializer(DataSource dataSource) {
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(new ClassPathResource(
+                "org/springframework/session/jdbc/schema-postgresql.sql"
+        ));
+        populator.setContinueOnError(true);
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(populator);
+        return initializer;
+    }
 }
